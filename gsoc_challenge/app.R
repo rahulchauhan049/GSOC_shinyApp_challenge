3f7a7bd97ad5c95d5652146585739bb9ceb7627a
RAHUL <rahul.chauhan049@gmail.com>
2019-05-30 20:20:23 +0530

    added comments

--- gsoc_challenge/app.R
+++ gsoc_challenge/app.R
@@ -4,23 +4,24 @@ req_packages <- pcakages[!(pcakages %in% installed.packages()[, "Package"])] # c
 if (length(req_packages) > 0) { # installing is needed
   install.packages(req_packages, dependencies = TRUE)
 }
-#Display
 sapply(pcakages, require, character.only = TRUE)
 
+#Import libraries
 library(shiny)
-#Download required tables from local system
+
+#Download required tables from local system.............................................
 a <- read.csv("a.csv");
 country <- read.csv("countrycode.csv");
 
-# Define UI for application that draws a histogram........................................................................................................
+# Define UI ........................................................................................................
 ui <- fluidPage(
+  #GSOC Image
   div(img(src="logo.png", style="margin-top: 10px; width: 120px; float: right;", height = 50)),
     # Application title
     titlePanel("GSOC Shiny Challenge"),
     theme=shinytheme("spacelab"),# returns URL of a shiny theme
     # themeSelector(),
     navbarPage(
-
       title = "Google Summer of code 2019",
       id="nav",
       tabPanel("Summary", value="Summary",
@@ -46,9 +47,10 @@ ui <- fluidPage(
                                               
       )#End of div
                ),
+      #Data Tab starts here..............................................................
       tabPanel("Data", value = "data", 
                sidebarLayout(
-                 
+                 #Sidebar panel of data tab......
                  sidebarPanel(conditionalPanel(condition = "input.datatabs==1",selectizeInput(
                    'sname', 'Search Scientific Name', choices = c("Mammalia", "Animalia", "Viruses", "Archaea", "incertae sedis",
                                                                   "Protozoa", "Bacteria", "Chromista", "Fungi", "Plantae"),
@@ -76,19 +78,20 @@ ui <- fluidPage(
                                     downloadButton(outputId = "databuttonrinat", label = "Download Data")))
                  ,
                  
-                 # Show a plot of the generated distribution
+                 #Main panel of data tab..........
                  mainPanel(
                    tabsetPanel(id="datatabs", tabPanel("GBIF",value = 1, dataTableOutput('table')),
                                   tabPanel("RINAT", value = 2, dataTableOutput('rinattable')))
                    
                  ))
 
-    # Sidebar with a slider input for number of bins 
-    
-        ),
+
+        ),#End of data tab
+      #starting of visualization Tab
     navbarMenu("Visualization",
                tabPanel("Spatial Visualizations",
                         sidebarLayout(
+                          #Sidebar of visualization tab
                           sidebarPanel(conditionalPanel(condition = "input.spatialvisualization==1",
                                                         selectizeInput(
                                                           'snamemapgrid', 'Search Scientific Name', choices = c("Mammalia", "Animalia", "Viruses", "Archaea", "incertae sedis",
@@ -103,6 +106,7 @@ ui <- fluidPage(
                                                                            onInitialize = I('function() { this.setValue(""); }')
                                                                          )),numericInput("limitmapgrid","Enter a search limit:",value = 40,min = 10,max = 1000000),
                                                         actionButton("searchmapgrid",label="Search || Update",styleclass="primary"),hr(), radioButtons("downtype", label = "Select file type", choices = c("JPG"="jpg", "PNG"="png", "PDF"="pdf"), inline = TRUE), downloadButton(outputId = "down", label = "Download Plot"))),
+                          #main panel of visualization tab
                           mainPanel(
                             tabsetPanel(id="spatialvisualization",
                               tabPanel("Mapgrid", value = 1, plotOutput("mapgrid")),
@@ -110,7 +114,8 @@ ui <- fluidPage(
                             )
                           )
                         )
-                        ),
+                        ),#End of spatial visualization tab
+               #Starting of Sub Tab Temporal visualization
                tabPanel("Temporal Visualizations", sidebarLayout(
                  sidebarPanel(conditionalPanel(condition = "input.temporalvisualization==1", textInput("t","qdq"))
                  ),
@@ -121,7 +126,8 @@ ui <- fluidPage(
                                tabPanel("Tempolar", value = 3)
                    )
                  )
-               )),
+               )),#End of temporal visualization tab
+               #Starting of Sub Tab Temporal visualization
                tabPanel("Taxonomic Visualizations", sidebarLayout(
                  sidebarPanel(conditionalPanel(condition = "input.taxonomicvisualization==1", textInput("t","qdq"))
                  ),
@@ -131,15 +137,16 @@ ui <- fluidPage(
                                tabPanel("WordCloud", value = 2)
                    )
                  )
-               ))
-               )
+               ))#End of spatial visualization tab
+               )#End of navbar menu called visualization
     
-    )
-)
+    )#End of navbar page
+)#End of fluidpage
 
-# Define 'server logic required to draw a histogram..........................................................................................................
+# Define 'server ..........................................................................................................
 server <- function(input, output, session) {
   
+  #For select all attritbute in gbif data
   observe({
     if("SelectAll" %in% input$fields)
       selected_choices=a[-1,] # choose all the choices _except_ "Select All"
@@ -154,17 +161,21 @@ server <- function(input, output, session) {
   spatialtype <- reactive({
     input$downtype
   })
-  #Download type of data
+  #Download gbif type of data
   fileext <- reactive({
     switch(input$dataradio,
            "Excel (CSV)" = "csv", "Text (TSV)" = "txt","Text (Space Separated)" = "txt", "Doc" = "doc")
     
   })
+  
+  
+  #Download rinat type of data
   fileextrinat <- reactive({
     switch(input$dataradiorinat,
            "Excel (CSV)" = "csv", "Text (TSV)" = "txt","Text (Space Separated)" = "txt", "Doc" = "doc")
     
   }) 
+  
   #Download function for spatial type plot
   output$down <- downloadHandler(filename = function(){
     paste("hist",spatialtype(), sep = ".")
@@ -186,6 +197,8 @@ server <- function(input, output, session) {
     sep <- switch(input$dataradio, "Excel (CSV)" = ",", "Text (TSV)" = "\t","Text (Space Separated)" = " ", "Doc" = " ")
     write.table(occ <- gbif(input$sname,input$limit,input$cntry, input$fields), file, sep = sep, row.names = FALSE)
   })
+  
+  
   #Download button for rinat
   output$databuttonrinat <- downloadHandler(filename = function(){
     paste(input$dataradiorinat, fileextrinat(), sep = ".")
@@ -193,27 +206,29 @@ server <- function(input, output, session) {
     sep <- switch(input$dataradiorinat, "Excel (CSV)" = ",", "Text (TSV)" = "\t","Text (Space Separated)" = " ", "Doc" = " ")
     write.table(inat<-inatdata(input$queryrinat, input$taxon_namerinat, input$maxrinat, input$year, input$month, input$date), file, sep = sep, row.names = FALSE)
   })
+  
+  
   observeEvent(input$searchrinat,{
-    
     output$rinattable <- renderDataTable(inat<-inatdata(input$queryrinat, input$taxon_namerinat, input$maxrinat, input$year, input$month, input$date))
   })
   
   #Output for Summart Tab
   output$summary <- renderText("Hello this is a text")
+  
   #Output for SPatial Tab
   output$hist = renderPlot(hist(c(1,2,3,4,5)))
+  
   #Output for Data Tab
   observeEvent(input$search, {
     output$table = renderDataTable(occ <- gbif(input$sname,input$limit,input$cntry, input$fields))
-  
   })
+  
   observeEvent(input$searchmapgrid, {
     output$mapgrid <- renderPlot({
       mapgridfunction(input$snamemapgrid, input$limitmapgrid, input$countrymapgrid)
     })
   })
-
-}
+}#END OF SERVER
 
 
 
@@ -234,10 +249,12 @@ mapgridfunction <- function(sname="Mammals", limit=40, cntry="world"){
 }
 
 #Data Download...........................................................................
+
 #rinat...................................................................................
 inatdata <- function(queries=NULL, taxon="reptileindia", max=50, year=NULL, month=NULL, day=NULL){
   inat<-get_inat_obs(query = queries, taxon_name = taxon, taxon_id = NULL, quality = NULL, geo = TRUE, year = year, month = month, day = day, bounds = NULL, maxresults = max, meta = FALSE)
 }
+
 #gbif....................................................................................
 gbif <- function(sname="Mammalia", olimit=10, cntry="world", fields){
   if(cntry=="world")
@@ -253,5 +270,6 @@ gbif <- function(sname="Mammalia", olimit=10, cntry="world", fields){
   }
   return(occ[fields])
 }
+
 # Run the application 
 shinyApp(ui = ui, server = server)
