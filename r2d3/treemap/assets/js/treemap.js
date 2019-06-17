7cd09af98ce055ab81960906a4dbc8206529898b
RAHUL <rahul.chauhan049@gmail.com>
2019-06-17 21:00:10 +0530

    added new visualization called treemap

---
+++ r2d3/treemap/assets/js/treemap.js
@@ -0,0 +1,93 @@
+// !preview r2d3 data = read.csv("data1.csv"), d3_version = 4
+
+// Based on: https://bl.ocks.org/mbostock/4063582
+
+var fader = function(color) { return d3.interpolateRgb(color, "#fff")(0.2); },
+    color = d3.scaleOrdinal(d3.schemeCategory20.map(fader)),
+    format = d3.format(",d");
+var stratify = d3.stratify()
+    .parentId(function (d) {
+        return d.id.substring(0, d.id.lastIndexOf("."));
+    });
+var treemap = d3.treemap()
+    .tile(d3.treemapResquarify)
+    .size([width, height])
+    .round(true)
+    .paddingInner(1);
+
+r2d3.onRender(function(data, svg, w, h, options) {
+  var root = stratify(data)
+  .sum(function (d) {
+    return d.value;
+})
+  .sort(function (a, b) {
+    return b.height - a.height || b.value - a.value;
+});
+
+  treemap(root);
+
+  var cell = svg.selectAll("g")
+    .data(root.leaves())
+    .enter().append("g")
+      .attr("transform", function(d) { return "translate(" + d.x0 + "," + d.y0 + ")"; });
+
+  cell.append("rect")
+      .attr("id", function(d) { return d.data.id; })
+      .attr("width", function(d) { return d.x1 - d.x0; })
+      .attr("height", function(d) { return d.y1 - d.y0; })
+      .attr("fill", function(d) { return color(d.parent.data.id); });
+
+  cell.append("clipPath")
+      .attr("id", function(d) { return "clip-" + d.data.id; })
+    .append("use")
+      .attr("xlink:href", function(d) { return "#" + d.data.id; });
+
+  cell.append("text")
+      .attr("clip-path", function(d) { return "url(#clip-" + d.data.id + ")"; })
+      .style("font-size", Math.min(width, height) / 50)
+    .selectAll("tspan")
+      .data(function(d) { return d.data.id.split(/(?=[A-Z][^A-Z])/g); })
+    .enter().append("tspan")
+      .attr("x", 4)
+      .attr("y", function(d, i) { return 13 + i * 1; })
+        .text(function(d) { 
+        var parts    = d.split('.');
+        var answer   = parts[parts.length - 1];
+        return answer
+        
+      })
+
+  cell.append("title")
+      .text(function(d) { return d.data.id + "\n" + format(d.value); });
+
+  d3.selectAll("input")
+      .data([sumBySize, sumByCount], function(d) { return d ? d.name : this.value; })
+      .on("change", changed);
+
+  var timeout = d3.timeout(function() {
+    d3.select("input[value=\"sumByCount\"]")
+        .property("checked", true)
+        .dispatch("change");
+  }, 2000);
+
+  function changed(sum) {
+    timeout.stop();
+
+    treemap(root.sum(sum));
+
+    cell.transition()
+        .duration(750)
+        .attr("transform", function(d) { return "translate(" + d.x0 + "," + d.y0 + ")"; })
+      .select("rect")
+        .attr("width", function(d) { return d.x1 - d.x0; })
+        .attr("height", function(d) { return d.y1 - d.y0; });
+  }
+});
+
+function sumByCount(d) {
+  return d.children ? 0 : 1;
+}
+
+function sumBySize(d) {
+  return d.size;
+}
