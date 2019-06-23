5e005d50995a03390db8e5db8fd384ac40d97616
RAHUL <rahul.chauhan049@gmail.com>
2019-06-24 00:25:38 +0530

    first reactive plots

--- interactive shiny/app.r
+++ interactive shiny/app.r
@@ -8,68 +8,96 @@ library("dplyr")
 library("rgbif")
 library("jsonlite")
 #Import Datasets
-data<- read.csv("data.csv")
+data <- read.csv("data.csv")
 
 
 #Shiny App starts from here............................
 ui <-  dashboardPage(
-  
-  title = "Interactive and reactive Shiny app experiment", skin = "purple",
+  title = "Interactive and reactive Shiny app experiment",
+  skin = "purple",
   dashboardHeader(title = "Interactive and reactive Shiny app experiment"),
   
   
   
-  dashboardSidebar(
-    sidebarMenu(
-      menuItem("Data", tabName = "data", icon = icon("table")),
-      menuSubItem("Data Visualization", tabName = "plots")
-      
-    )
-  ),
+  dashboardSidebar(sidebarMenu(
+    menuItem("Data", tabName = "data", icon = icon("table")),
+    menuSubItem("Data Visualization", tabName = "plots")
+    
+  )),
   
   
-  dashboardBody(
-    tabItems(
+  dashboardBody(tabItems(
     tabItem(tabName = "data",
-              fluidRow(h1("Records from gbif."),
-                       br(), h3("Some recoreds with globally spread occurance."), br(), br()),
-              fluidRow(tabPanel(title = "Data",status = "primary",solidHeader = T, dataTableOutput('table'), background = "aqua"))
-      ),
-      
-      tabItem(tabName = "plots",
-              fluidRow(
-                column(1,
-              verbatimTextOutput("selected")),
-              ),fluidRow(
-              d3Output("d3"),
-              d3Output("map")
+            fluidRow(
+              h1("Records from gbif."),
+              br(),
+              h3("Some recoreds with globally spread occurance."),
+              br(),
+              br()
+            ),
+            fluidRow(
+              tabPanel(
+                title = "Data",
+                status = "primary",
+                solidHeader = T,
+                dataTableOutput('table'),
+                background = "aqua"
               )
-                  )
+            )),
+    
+    tabItem(
+      tabName = "plots",
+      fluidRow(verbatimTextOutput("selected"),
+               d3Output("d3")),
+      plotOutput("map")
       
     )
-  )
+    
+  ))
 )
 
 
 # server
-server <- function(input, output){
+server <- function(input, output) {
   output$table = renderDataTable(data)
   #Here i have called shiny module that i made to create word cloud.....
   #callModue(wordOutput, "same name that used in UI part")
   output$d3 <- renderD3({
     r2d3(
       data = read.csv("data1.csv"),
-      css = "circlepacking.css", d3_version = 4, script = "circlepacking.js"
+      css = "circlepacking.css",
+      d3_version = 4,
+      script = "circlepacking.js"
     )
   })
   
-  observeEvent(input$bar_clicked,{
-    output$map <- renderD3(d3_map(data, name = input$bar_clicked))
+  observeEvent(input$bar_clicked, {
+    output$selected <- renderText(input$bar_clicked)
+    output$map <- renderPlot({
+      {data<- read.csv("data.csv")
+        data <- format_bdvis(data,source='rgbif')
+        data1 <- data[c("Longitude", "Latitude", "order")]
+        clicked <- unlist(strsplit(input$bar_clicked, "\\."))
+        if(length(clicked)>2){
+        clicked <- tail(clicked, n=1)
+        data1 <- data1 %>% filter(order == clicked)
+        }
+        data1 <- format_bdvis(data1,source='rgbif')
+        mapgrid(indf = data1, ptype = "records", legscale = 0, collow = "blue", colhigh = "red",
+                mapdatabase = "world", gridscale = 1 )
+        
+      }
+    })
   })
   
-  output$map <- renderD3(
-    d3_map(data)
-  )
+  output$map <- renderPlot({
+    data <- format_bdvis(data,source='rgbif')
+    data1 <- data[c("Longitude", "Latitude")]
+    data1 <- format_bdvis(data1,source='rgbif')
+    mapgrid(indf = data1, ptype = "records", title = "Mammals", legscale = 0, collow = "blue", colhigh = "red",
+            mapdatabase = "world", gridscale = 1)
+    
+  })
 }
 
 
@@ -110,29 +138,35 @@ hierarchy <- function(data) {
   names(id)[2] <- paste("value")
   return(rbind(a, b, c, id))
 }
-d3_map <- function(data, map="world", name=NA){
-  data<- data[c("decimalLatitude", "decimalLongitude", "order")]
-  data<- data<-na.omit(data)
+d3_map <- function(data, map = "world", name = NA) {
+  data <- data[c("decimalLatitude", "decimalLongitude", "order")]
+  data <- data <- na.omit(data)
   
-  if(is.na(name)){
-  }else{
-    clicked <- tail(unlist(strsplit(name, "\\.")),n=1)
+  if (is.na(name)) {
+    
+  } else{
+    clicked <- tail(unlist(strsplit(name, "\\.")), n = 1)
     data <- data %>% filter(order == clicked)
   }
   names(data)[names(data) == "order"] <- "name"
   modified <- ""
-  modified <- list(
-    traits = colnames(data),
-    values = data
-  )
-  modified<-jsonlite::toJSON(modified)
-  if(map=="world"){
-    r2d3::r2d3(data = c(jsonlite::read_json("assets/json/worldmap.json"),jsonlite::parse_json(modified)),
-               css = "assets/css/map.css",
-               d3_version = 3,
-               dependencies = c("assets/js/topojson.min.js",
-                                "http://labratrevenge.com/d3-tip/javascripts/d3.tip.v0.6.3.js"),
-               script = "assets/js/worldmap.js")
+  modified <- list(traits = colnames(data),
+                   values = data)
+  modified <- jsonlite::toJSON(modified)
+  if (map == "world") {
+    r2d3::r2d3(
+      data = c(
+        jsonlite::read_json("assets/json/worldmap.json"),
+        jsonlite::parse_json(modified)
+      ),
+      css = "assets/css/map.css",
+      d3_version = 3,
+      dependencies = c(
+        "assets/js/topojson.min.js",
+        "http://labratrevenge.com/d3-tip/javascripts/d3.tip.v0.6.3.js"
+      ),
+      script = "assets/js/worldmap.js"
+    )
   }
 }
 
