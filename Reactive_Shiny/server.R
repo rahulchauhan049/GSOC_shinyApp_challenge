578b45f9455c7b3c53bfdb85e88c89d4d52fc388
RAHUL <rahul.chauhan049@gmail.com>
2019-07-01 17:26:11 +0530

    feature to select, upload, download data

---
+++ Reactive_Shiny/server.R
@@ -0,0 +1,135 @@
+#Importing packages
+library("rgbif")
+library("bdvis")
+library("rinat")
+library("shinythemes")
+library(shiny)
+#import Datasets
+a <- read.csv("a.csv")
+country <- read.csv("countrycode.csv")
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output, session) {
+    #For select all attritbute in gbif data
+    observe({
+        if("SelectAll" %in% input$fields)
+            selected_choices=a[-1,] # choose all the choices _except_ "Select All"
+        else
+            selected_choices=input$fields # update the select input with choice selected by user
+        updateSelectInput(session, "fields", selected = selected_choices)
+    })
+    
+    output$tableoutput <- DT::renderDataTable({
+        tryCatch(
+            {
+                df <- read.csv(paste("www/csv/",input$datasetselected,sep=""),
+                               header = input$header,
+                               sep = input$sep,
+                               quote = input$quote)
+            },
+            error = function(e) {
+                # return a safeError if a parsing error occurs
+                stop(safeError(e))
+            }
+        )
+        if(input$disp == "head") {
+            return(head(df))
+        }
+        else {
+            return(df)
+        }
+        
+    })
+    output$tableupload <- DT::renderDataTable({
+        # input$file1 will be NULL initially. After the user selects
+        # and uploads a file, head of that data file by default,
+        # or all rows if selected, will be shown.
+        
+        req(input$file1)
+        
+        # when reading semicolon separated files,
+        # having a comma separator causes `read.csv` to error
+        tryCatch(
+            {
+                df <- read.csv(input$file1$datapath,
+                               header = input$header,
+                               sep = input$sep,
+                               quote = input$quote)
+            },
+            error = function(e) {
+                # return a safeError if a parsing error occurs
+                stop(safeError(e))
+            }
+        )
+        
+        if(input$disp == "head") {
+            return(head(df))
+        }
+        else {
+            return(df)
+        }
+        
+    })
+    
+    
+    
+    #Download gbif type of data
+    fileext <- reactive({
+        switch(input$dataradio,
+               "Excel (CSV)" = "csv", "Text (TSV)" = "txt","Text (Space Separated)" = "txt", "Doc" = "doc")
+        
+    })
+    #Download rinat type of data
+    fileextrinat <- reactive({
+        switch(input$dataradiorinat,
+               "Excel (CSV)" = "csv", "Text (TSV)" = "txt","Text (Space Separated)" = "txt", "Doc" = "doc")
+        
+    }) 
+    #Download button for Data
+    output$databutton <- downloadHandler(filename = function(){
+        paste(input$dataradio, fileext(), sep = ".")
+    }, content = function(file){
+        sep <- switch(input$dataradio, "Excel (CSV)" = ",", "Text (TSV)" = "\t","Text (Space Separated)" = " ", "Doc" = " ")
+        write.table(occ <- gbif(input$sname,input$limit,input$cntry, input$fields), file, sep = sep, row.names = FALSE)
+    })
+    
+    
+    #Download button for rinat
+    output$databuttonrinat <- downloadHandler(filename = function(){
+        paste(input$dataradiorinat, fileextrinat(), sep = ".")
+    }, content = function(file){
+        sep <- switch(input$dataradiorinat, "Excel (CSV)" = ",", "Text (TSV)" = "\t","Text (Space Separated)" = " ", "Doc" = " ")
+        write.table(inat<-inatdata(input$queryrinat, input$taxon_namerinat, input$maxrinat, input$year, input$month, input$date), file, sep = sep, row.names = FALSE)
+    })
+    observeEvent(input$searchrinat,{
+        output$rinattable <- renderDataTable(inat<-inatdata(input$queryrinat, input$taxon_namerinat, input$maxrinat, input$year, input$month, input$date))
+    })
+    
+    observeEvent(input$search, {
+        output$table = renderDataTable(occ <- gbif(input$sname,input$limit,input$cntry, input$fields))
+    })
+    
+})#END OF SERVER
+
+#Data Download...........................................................................
+
+#rinat...................................................................................
+inatdata <- function(queries=NULL, taxon="reptileindia", max=50, year=NULL, month=NULL, day=NULL){
+    inat<-get_inat_obs(query = queries, taxon_name = taxon, taxon_id = NULL, quality = NULL, geo = TRUE, year = year, month = month, day = day, bounds = NULL, maxresults = max, meta = FALSE)
+}
+
+#gbif....................................................................................
+gbif <- function(sname="Mammalia", olimit=10, cntry="world", fields){
+    if(cntry=="world")
+        cntry=NULL
+    key <- name_backbone(name = sname)$usageKey
+    occ <- occ_search(taxonKey = key,country = cntry, limit = olimit,
+                      year = "2017,2018" ,return = "data")
+    if(is.null(fields)){
+        fields=c("class","family", "genus", "species")
+    }
+    else{
+        fields = fields
+    }
+    return(occ[fields])
+}
---
+++ Reactive_Shiny/ui.R
@@ -0,0 +1,271 @@
+#Importing packages
+library("DT")
+library("rgbif")
+library("bdvis")
+library("rinat")
+library("shinythemes")
+library("shiny")
+# Define UI for application
+a <- read.csv("a.csv")
+country <- read.csv("countrycode.csv")
+
+shinyUI(fluidPage(
+    #GSOC Image
+    # Application title
+    titlePanel("GSOC Shiny Challenge"),
+    theme = shinytheme("spacelab"),
+    # returns URL of a shiny theme
+    # themeSelector(),
+    navbarPage(
+        title = "Google Summer of code 2019",
+        id = "nav",
+        tabPanel(
+            "Summary",
+            value = "Summary",
+            tags$div(
+                id = "profile",
+                style = "
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    padding: 8px;
+    border: 2px solid #ccc;
+    border-radius: 5px; /* 5px rounded corners */
+    max-width: 800px;
+    margin: 5% auto;
+    color: black;
+    background: rgba(255, 255, 255, 0.8);
+  ",
+                h3("Welcome to bdvis"),
+                hr(),
+                h4("Data"),
+                p(
+                    "This shiny package allows you to use functioning of bdvis package in an interactive manner. This shiny app allows you to download dataset from gbif.You can select various parameters through graphical pannels. "
+                ),
+                hr(),
+                h4("Visualizations"),
+                p(
+                    "This shiny also allows you to use various visualization functions on the dataset that you have downloaded"
+                ),
+                hr(),
+                p("To start, click in 'data' tab on the top navigation bar")
+                
+            )#End of div
+        ),
+        #Data Tab starts here..............................................................
+        tabPanel("Data", value = "data",
+                 sidebarLayout(
+                     #Sidebar panel of data tab......
+                     sidebarPanel(
+                         conditionalPanel(
+                             condition = "input.datatabs==1",
+                             selectInput(
+                                 "datasetselected",
+                                 "Choose from existing dataset",
+                                 c("Mammals" = "sampledata.csv", "Hyena" = "hyenaData.csv")
+                             ),
+                             
+                             # Horizontal line ----
+                             tags$hr(),
+                             
+                             # Input: Checkbox if file has header ----
+                             checkboxInput("header", "Header", TRUE),
+                             
+                             # Input: Select separator ----
+                             radioButtons(
+                                 "sep",
+                                 "Separator",
+                                 choices = c(
+                                     Comma = ",",
+                                     Semicolon = ";",
+                                     Tab = "\t"
+                                 ),
+                                 selected = ","
+                             ),
+                             
+                             # Input: Select quotes ----
+                             radioButtons(
+                                 "quote",
+                                 "Quote",
+                                 choices = c(
+                                     None = "",
+                                     "Double Quote" = '"',
+                                     "Single Quote" = "'"
+                                 ),
+                                 selected = '"'
+                             ),
+                             
+                             # Horizontal line ----
+                             tags$hr(),
+                             
+                             # Input: Select number of rows to display ----
+                             radioButtons(
+                                 "disp",
+                                 "Display",
+                                 choices = c(Head = "head",
+                                             All = "all"),
+                                 selected = "head"
+                             )
+                         ),
+                         conditionalPanel(
+                             condition = "input.datatabs==2",
+                             fileInput(
+                                 "file1",
+                                 "Choose CSV File",
+                                 multiple = FALSE,
+                                 accept = c("text/csv",
+                                            "text/comma-separated-values,text/plain",
+                                            ".csv")
+                             ),
+                             
+                             # Horizontal line ----
+                             tags$hr(),
+                             
+                             # Input: Checkbox if file has header ----
+                             checkboxInput("header", "Header", TRUE),
+                             
+                             # Input: Select separator ----
+                             radioButtons(
+                                 "sep",
+                                 "Separator",
+                                 choices = c(
+                                     Comma = ",",
+                                     Semicolon = ";",
+                                     Tab = "\t"
+                                 ),
+                                 selected = ","
+                             ),
+                             
+                             # Input: Select quotes ----
+                             radioButtons(
+                                 "quote",
+                                 "Quote",
+                                 choices = c(
+                                     None = "",
+                                     "Double Quote" = '"',
+                                     "Single Quote" = "'"
+                                 ),
+                                 selected = '"'
+                             ),
+                             
+                             # Horizontal line ----
+                             tags$hr(),
+                             
+                             # Input: Select number of rows to display ----
+                             radioButtons(
+                                 "disp",
+                                 "Display",
+                                 choices = c(Head = "head",
+                                             All = "all"),
+                                 selected = "head"
+                             )
+                         ),
+                         conditionalPanel(
+                             condition = "input.datatabs==3",
+                             selectizeInput(
+                                 'sname',
+                                 'Search Scientific Name',
+                                 choices = c(
+                                     "Mammalia",
+                                     "Animalia",
+                                     "Viruses",
+                                     "Archaea",
+                                     "incertae sedis",
+                                     "Protozoa",
+                                     "Bacteria",
+                                     "Chromista",
+                                     "Fungi",
+                                     "Plantae"
+                                 ),
+                                 options = list(
+                                     placeholder = 'Please select an option below',
+                                     onInitialize = I('function() { this.setValue(""); }')
+                                 )
+                             ),
+                             selectizeInput(
+                                 "cntry",
+                                 "Select Country",
+                                 choices = country[2],
+                                 options = list(
+                                     placeholder = 'Please select Country',
+                                     onInitialize = I('function() { this.setValue(""); }')
+                                 )
+                             ),
+                             selectizeInput(
+                                 'fields',
+                                 'Select Attributes to be displayed',
+                                 choices = a ,
+                                 multiple = TRUE
+                             ),
+                             numericInput(
+                                 "limit",
+                                 "Enter a search limit:",
+                                 value = 10,
+                                 min = 10,
+                                 max = 1000000
+                             ),
+                             actionButton("search", label = "Search || Update", styleclass =
+                                              "primary"),
+                             hr(),
+                             "Click on the download button to download dataset observation",
+                             radioButtons(
+                                 "dataradio",
+                                 label = "Select file type",
+                                 choices = c("Excel (CSV)", "Text (TSV)", "Text (Space Separated)", "Doc"),
+                                 inline = TRUE
+                             ),
+                             downloadButton(outputId = "databutton", label = "Download Data")
+                             
+                         ),
+                         conditionalPanel(
+                             condition = "input.datatabs==4",
+                             textInput("queryrinat", "Query"),
+                             textInput("taxon_namerinat", "taxon_name"),
+                             numericInput("maxrinat", "No. of Obervations", value = 50),
+                             numericInput("year", "Year", value = 2019),
+                             numericInput(
+                                 "month",
+                                 "Month",
+                                 value = 01,
+                                 min = 01,
+                                 max = 12
+                             ),
+                             numericInput(
+                                 "date",
+                                 "Date",
+                                 value = 01,
+                                 min = 01,
+                                 max = 30
+                             ),
+                             actionButton("searchrinat", label =
+                                              "Search || Update", styleclass = "primary"),
+                             hr(),
+                             "Click on the download button to download dataset observation",
+                             radioButtons(
+                                 "dataradiorinat",
+                                 label = "Select file type",
+                                 choices = c("Excel (CSV)", "Text (TSV)", "Text (Space Separated)", "Doc"),
+                                 inline = TRUE
+                             ),
+                             downloadButton(outputId = "databuttonrinat", label = "Download Data")
+                         )
+                     ),
+                     
+                     
+                     
+                     #Main panel of data tab..........
+                     mainPanel(
+                         tabsetPanel(
+                             id = "datatabs",
+                             tabPanel(
+                                 "Existing Dataset",
+                                 value = 1,
+                                 DT::dataTableOutput("tableoutput")
+                             ),
+                             tabPanel("Upload Dataset", value = 2, DT::dataTableOutput("tableupload")),
+                             tabPanel("GBIF", value = 3, dataTableOutput('table')),
+                             tabPanel("RINAT", value = 4, dataTableOutput('rinattable'))
+                         )
+                     )
+                 )))#End of navbar menu called visualization
+        
+    )#End of navbar page
+)#End of fluidpage)
